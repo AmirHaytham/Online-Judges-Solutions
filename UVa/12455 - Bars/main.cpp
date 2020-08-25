11d66a4ca01e6a50330e81c7b618c48712474992
Amir Haytham Salama <amir.haytham.salama@gmail.com>
2020-08-25 20:34:07 +0200

    Update main.cpp

--- UVa/12455 - Bars/main.cpp
+++ UVa/12455 - Bars/main.cpp
@@ -7,22 +7,22 @@ int main()
     int TC ; cin >> TC ;
     while(TC--){
         int required ; cin >> required ;
-        int n ; bool ok = 1 ; cin >> n ;
+        int n; bool ok = 1; cin >> n;
         vector<int> v(n) ;
-        for(int i = 0 ; i < n ; ++i)cin >> v[i] ;
+        for(int i = 0; i < n; ++i)cin >> v[i];
 
-        for(int i = 0 ; i < (1 << n) ; ++i){
+        for(int i = 0; i < (1 << n); ++i){
             int sum = 0 ;
-            for(int j = 0 ; j < n ; ++j){
-                if (i & (1 << j) )
+            for(int j = 0; j < n; ++j){
+                if (i & (1 << j))
                     sum += v[j] ;
             }
             if(sum == required){
-                cout << "YES\n" ;
-                ok = 0 ;
-                break ;
+                cout << "YES\n";
+                ok = 0;
+                break;
             }
         }
-        if (ok)cout << "NO\n" ;
+        if (ok)cout << "NO\n";
     }
 }
